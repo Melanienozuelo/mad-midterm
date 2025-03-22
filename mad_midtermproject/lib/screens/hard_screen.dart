ec8c0620c663cd2a2026e964b24a70b36a16d99c
Melanienozuelo <melanienozuelo183@gmail.com>
2025-03-22 16:41:25 +0800

    mtr

---
+++ mad_midtermproject/lib/screens/hard_screen.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+
+class HardLevelScreen extends StatelessWidget {
+  const HardLevelScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Mode'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange.shade900,
+        foregroundColor: Colors.white,
+        actions: const [
+          Icon(Icons.volume_up_rounded),
+          SizedBox(
+              width: 8,
+            ),
+        ],
+      ),
+      body: Stack(
+        children: [
+           Image.asset('assets/image/background.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+            ),
+         Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,                  
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/buko.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/lechon.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                    height: 100,
+                    width: 70,
+                    child: Image.asset(
+                    'assets/image/pica-pic.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                 ),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/bibingka.jpg',
+                    fit: BoxFit.fill,
+                    ),                  
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/bibingka.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                    height: 100,
+                    width: 70,
+                    child: Image.asset(
+                    'assets/image/puto.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                 ),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/buko.jpg',
+                    fit: BoxFit.fill,
+                    ),                
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/puto.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                    height: 100,
+                    width: 70,
+                 ),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                  child: Image.asset(
+                    'assets/image/pica-pic.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                  height: 100,
+                  width: 70,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                 Container(
+                  color: Colors.deepOrange.shade900,
+                    height: 100,
+                    width: 70,
+                    child: Image.asset(
+                    'assets/image/lechon.jpg',
+                    fit: BoxFit.fill,
+                    ),
+                 ),
+              ],
+            ),
+            ],
+          ),
+        ],
+      ),  
+    );
+  }
+}
